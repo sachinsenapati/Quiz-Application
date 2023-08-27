c812c7ed1ef89c2c98283ee47ffa7316588ace21
Sachin-senapati <sachinsenapati05@gmail.com>
2023-08-27 16:11:06 +0530

    db updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,23 +1,10 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://ssb520:yWPQuwKQ34ILmZCX@cluster0.vtkz9ds.mongodb.net/mern-quiz?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
+const mongoose = require("mongoose");
 const connecton = async () => {
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    await client.close();
+    const conn = await mongoose.connect(process.env.CONNECTION);
+    console.log("connected to mongoDb");
+  } catch (error) {
+    console.log(`Error in mongoDb ${error}`);
   }
 };
 module.exports = connecton;
